6392a289cc7af5b4b06d00d0955c2e89f4c07d1f
Sharath R <sharath.r@mountblue.io>
2019-01-19 16:44:28 +0530

    code ship

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ec2-user',
-      key: '/home/sp/Desktop/drowssap.pem',
+      key: 'drowssap.pem',
       host: '52.66.237.135',
       ref: 'origin/master',
       repo: 'git@gitlab.com:mountblue/dec-2018-js-backend/08-sharath-mysql-api.git',
