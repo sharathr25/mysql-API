76d81b1645e2fac2609fe8640597a3c8aa04454a
Sharath R <sharath.r@mountblue.io>
2019-01-19 10:46:45 +0530

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       repo: 'git@gitlab.com:mountblue/dec-2018-js-backend/08-sharath-mysql-api.git',
       path: '/home/ec2-user/mysql-API/',
       'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'post-setup': 'pm2 start ecosystem.config.js',
+      'post-setup': 'pm2 start',
     },
   },
 };
