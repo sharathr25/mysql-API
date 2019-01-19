644d8359a4446d36e3a555d007b4cbaaef1ead82
Sharath R <sharath.r@mountblue.io>
2019-01-19 11:04:15 +0530

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,17 @@ module.exports = {
   apps: [{
     name: 'mysql-API',
     script: 'server.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
       NODE_ENV: 'production',
     },
   }],
@@ -14,9 +24,8 @@ module.exports = {
       host: '52.66.237.135',
       ref: 'origin/master',
       repo: 'git@gitlab.com:mountblue/dec-2018-js-backend/08-sharath-mysql-api.git',
-      path: '/home/ec2-user/mysql-API/',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
-      'post-setup': 'pm2 start ecosystem.config.js',
+      path: '/home/ec2-user/var/www/production/mysql-API',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production && npm start',
     },
   },
 };
